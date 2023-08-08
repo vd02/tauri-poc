784f71b91683eb58daa21fe6930ac02b890f1691
Varun Dixit <dixitvarun0208@gmail.com>
2023-08-09 02:23:51 +0530

    CaseLawsIndex Search (Heading + Subheading) add

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import cors from "cors";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
@@ -13,82 +12,55 @@ const App: React.FC = () => {
   };
 
   const handleGoButtonClick = () => {
-    var data = {
-      add: {
-        doc: {
-          id: inputValue,
-          title: "Doc 1"
-        }
-      }
-    };
-
-    var jsonData = JSON.stringify(data);
-    var solrUrl = "http://localhost:8983/solr/techproducts/update?commit=true";
-
-    fetch(solrUrl, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: jsonData
-    })
-      .then(function(response) {
-        if (response.ok) {
-          toast.success("Value inserted"); // Show success toast message
-          console.log("Data posted successfully to Solr");
-        } else {
-          toast.error("Failed to insert value"); // Show error toast message
-          console.log("Failed to post data to Solr");
-        }
-      })
-      .catch(function(error) {
-        console.log("Error:", error);
-      });
-  };
-
-  const handleGetDataClick = () => {
-    var solrUrl = "http://localhost:8983/solr/techproducts/select?q=*%3A*&rows=100";
+    const solrBaseUrl = "http://localhost:8983/solr/gst-excus-sample3/select";
+    const queryParams = new URLSearchParams({
+      q: `heading:"${inputValue}" OR subheading:"${inputValue}"`, // Search in heading or subheading fields
+      rows: "101",
+    });
+    const solrUrl = `${solrBaseUrl}?${queryParams}`;
 
     fetch(solrUrl)
-      .then(response => response.json())
-      .then(result => {
+      .then((response) => response.json())
+      .then((result) => {
         setData(result.response.docs);
-        toast.success("Data retrieved successfully"); // Show success toast message
+        toast.success("Search results retrieved successfully");
       })
-      .catch(error => {
-        toast.error("Failed to retrieve data"); // Show error toast message
+      .catch((error) => {
+        toast.error("Failed to retrieve search results");
         console.log("Error:", error);
       });
   };
+
 
   return (
     <div className="container">
-      <h1>Apache Solr + Tauri POC</h1>
+      <h1 style={{ textAlign: "center", marginTop: "20px" }}>CaseLawsIndex Search (Heading + Subheading)</h1>
       <div className="input-container">
         <input type="text" value={inputValue} onChange={handleInputChange} />
-        <button onClick={handleGoButtonClick}>Go</button>
+        <button onClick={handleGoButtonClick}>Search</button>
       </div>
-      <button style={{alignSelf:"center"}} onClick={handleGetDataClick}>Get Data</button>
-      <ul className="data-list">
-        {data.map(doc => (
-          <li key={doc.id}>
-            <h3>{doc.title}</h3>
-            <ul>
-              {Object.keys(doc).map(key => (
-                <li key={key}>
-                  <strong>{key}: </strong> {doc[key]}
-                </li>
+      <table className="data-table">
+        <thead>
+          <tr>
+            {data.length > 0 &&
+              Object.keys(data[0]).map((key) => (
+                <th key={key}>{key}</th>
               ))}
-            </ul>
-          </li>
-        ))}
-      </ul>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((doc, index) => (
+            <tr key={index}>
+              {Object.values(doc).map((value: any, idx) => (
+                <td key={idx}>{value}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
       <ToastContainer position="top-center" autoClose={3000} hideProgressBar={true} limit={1} />
     </div>
   );
 };
-
-// Enable CORS in the Express server
-cors();
 
 export default App;
